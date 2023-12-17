e2a4a7ff14741e1db4fbb6ad3c94f49d31fa9345
Olen <olen2003@gmail.com>
2023-12-17 14:32:35 +0200

    testinis commit'as

--- pages/interactive.tsx
+++ pages/interactive.tsx
@@ -2,7 +2,7 @@
 export default function Interactive() {
     return (
         <div className="flex items-center justify-center h-screen bg-gray-800">
-            <h1 className="text-white text-5xl font-bold mb-5">Interactive webpage</h1>
+            <h1 className="text-white text-5xl font-bold mb-5">blahblahblah</h1>
         </div>
     )
 }
