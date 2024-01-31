bb1b819e560e8925838809929bf35bab315bbddd
Olen Račkauskas <39692726+Sc0rpie@users.noreply.github.com>
2024-01-31 10:37:13 +0000

    Back button added to go to / (will be changed later)

--- src/pages/interactive.tsx
+++ src/pages/interactive.tsx
@@ -1,10 +1,28 @@
-import corkboardImage from '../images/corkboard.jpg'
+import { useNavigate } from "react-router-dom";
+import corkboardImage from "../images/corkboard.jpg";
 
 function Interactive() {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
   return (
     <>
       {/* Corkboard Section */}
       <div className="flex flex-col items-center justify-center h-screen bg-cover bg-center bg-[url(../images/corkboard.jpg)]">
+
+        {/* Back Arrow */}
+        <div className="absolute top-4 left-4">
+          <button
+            onClick={goBack}
+            className="text-2xl p-2 hover:bg-gray-200 rounded-full"
+          >
+            ←
+          </button>
+        </div>
+        
         <h1 className="text-5xl font-bold">Place for corkboard</h1>
       </div>
 
