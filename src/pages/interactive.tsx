d21dbb1bd2d8ffdcc3e9714ef8a04f0a25e3670a
Olen Račkauskas <39692726+Sc0rpie@users.noreply.github.com>
2024-01-31 10:29:33 +0000

    added corkboard image

--- src/pages/interactive.tsx
+++ src/pages/interactive.tsx
@@ -1,8 +1,10 @@
+import corkboardImage from '../images/corkboard.jpg'
+
 function Interactive() {
   return (
     <>
       {/* Corkboard Section */}
-      <div className="flex flex-col items-center justify-center h-screen bg-cover bg-center opacity-50">
+      <div className="flex flex-col items-center justify-center h-screen bg-cover bg-center bg-[url(../images/corkboard.jpg)]">
         <h1 className="text-5xl font-bold">Place for corkboard</h1>
       </div>
 
