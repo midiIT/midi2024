ac78d1f3fd5ff5b41d94f8ffa6baeb6b5c5273e5
gustaz <gustas2001@mail.ru>
2024-03-07 01:51:37 +0200

    Fix fonts, temp file structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Dosis", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
