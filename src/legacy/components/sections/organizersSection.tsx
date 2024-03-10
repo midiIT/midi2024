53615e6b79ae507cb527eb17c88daacd880f0914
Olen Račkauskas <39692726+Sc0rpie@users.noreply.github.com>
2024-03-10 17:26:28 +0000

    Most of the people added

--- src/legacy/components/sections/organizersSection.tsx
+++ src/legacy/components/sections/organizersSection.tsx
@@ -33,7 +33,7 @@ export default function OrganizersSection() {
           name="Augustina Petraitytė"
           title="MIDI vadovai"
           imgsrc={Gabija}
-          phone="+370 61399808"
+          phone="+370 61 399 808"
           email="vadovas@midi.lt"
           placeholder={"blur"}
         />
@@ -49,19 +49,19 @@ export default function OrganizersSection() {
         <Avatar
           className="basis-full md:basis-1/3 lg:basis-1/4"
           name="Gytis Mockevičius"
-          title="MC"
+          title="Minecraft vadovas"
           imgsrc={Ricardas}
-          phone="+370 67 420 546"
-          email="dienine@midi.lt"
+          phone="+370 62 423 266"
+          email="minecraft@midi.lt"
           placeholder={"blur"}
         />
         <Avatar
           className="basis-full md:basis-1/3 lg:basis-1/4"
           name="Domas Bruišys"
-          title="LAN"
+          title="LAN Party vadovas"
           imgsrc={Meta}
-          phone="+370 60 063 381"
-          email="vakarine@midi.lt"
+          phone="+370 60 313 085"
+          email="lan@midi.lt"
           placeholder={"blur"}
         />
         <Avatar
@@ -69,67 +69,220 @@ export default function OrganizersSection() {
           name="Rokas Baltrušaitis"
           title="Marketingo vadovas"
           imgsrc={Gytis}
-          phone="+370 62 423 266"
-          email="lan@midi.lt"
-          placeholder={"blur"}
-        />
-        <Avatar
-          className="basis-full md:basis-1/3 lg:basis-1/4"
-          name="Augustas Švetkauskas"
-          title="LAN Party vadovas"
-          imgsrc={Darius}
-          phone="+370 60 503 379"
-          email="lan@midi.lt"
-          placeholder={"blur"}
-        />
-        <Avatar
-          className="basis-full md:basis-1/3 lg:basis-1/4"
-          name="Gustas Štaševskis"
-          title="Informacinių technologijų vadovas"
-          imgsrc={Gustas}
-          phone="+370 62 466 600"
-          email="it@midi.lt"
-          placeholder={"blur"}
-        />
-        <Avatar
-          className="basis-full md:basis-1/3 lg:basis-1/4"
-          name="Rasa Škiudaitė"
-          title="Marketingo vadovė"
-          imgsrc={Rasa}
-          phone="+370 67 877 453"
+          phone="+370 67 543 514"
           email="marketingas@midi.lt"
           placeholder={"blur"}
         />
         <Avatar
           className="basis-full md:basis-1/3 lg:basis-1/4"
-          name="Liudas Norkevičius"
-          title="Barterinių remėjų vadovas"
+          name="Augustas Švetkauskas"
+          title="Organizacinės srities vadovas"
+          imgsrc={Darius}
+          phone="+370 62 213 302"
+          email="org@midi.lt"
+          placeholder={"blur"}
+        />
+        <Avatar
+          className="basis-full md:basis-1/3 lg:basis-1/4"
+          name="Olen Račkauskas"
+          title="Informacinių technologijų vadovas"
+          imgsrc={Gustas}
+          phone="+370 64 622 318"
+          email="it@midi.lt"
+          placeholder={"blur"}
+        />
+        <Avatar
+          className="basis-full md:basis-1/3 lg:basis-1/4"
+          name="Titas Masys"
+          title="Roko Operos vadovas"
+          imgsrc={Rasa}
+          phone="+370 69 535 871"
+          email="ro.vadovas@midi.lt"
+          placeholder={"blur"}
+        />
+        <Avatar
+          className="basis-full md:basis-1/3 lg:basis-1/4"
+          name="Vaiva Janavičiūtė"
+          title="Organizacinės srities vadovė"
           imgsrc={Liudas}
-          phone="+370 62 456 084"
+          phone="+370 65 653 406"
+          email="org@midi.lt"
+          placeholder={"blur"}
+        />
+        <Avatar
+          className="basis-full md:basis-1/3 lg:basis-1/4"
+          name="Mantas Globys"
+          title="Organizacinės srities vadovas"
+          imgsrc={Austeja}
+          phone="+370 69 368 981"
+          email="org@midi.lt"
+          placeholder={"blur"}
+        />
+        <Avatar
+          className="basis-full md:basis-1/3 lg:basis-1/4"
+          name="Nojus Jokūbaitis"
+          title="Kūrybinės grupės vadovas"
+          imgsrc={Austeja}
+          phone="+370 61 788 075"
+          email="kurybine@midi.lt"
+          placeholder={"blur"}
+        />
+        <Avatar
+          className="basis-full md:basis-1/3 lg:basis-1/4"
+          name="Mėja Plytnikaitė"
+          title="Administratorė"
+          imgsrc={Austeja}
+          phone="+370 67 978 534"
+          email="admin@midi.lt"
+          placeholder={"blur"}
+        />
+        <Avatar
+          className="basis-full md:basis-1/3 lg:basis-1/4"
+          name="Rūta Skergelzaitė"
+          title="Komunikacijos vadovė"
+          imgsrc={Austeja}
+          phone="+370 63 818 273"
+          email="org@midi.lt"
+          placeholder={"blur"}
+        />
+        <Avatar
+          className="basis-full md:basis-1/3 lg:basis-1/4"
+          name="Saule Gylyte"
+          title="Barterinių remėjų vadovė"
+          imgsrc={Austeja}
+          phone="+370 60 940 829"
           email="reklama@midi.lt"
           placeholder={"blur"}
         />
         <Avatar
           className="basis-full md:basis-1/3 lg:basis-1/4"
-          name="Austėja Bendikaitė"
-          title="Administratorė"
-          imgsrc={Austeja}
-          phone="+370 63 868 858"
-          email="admin@midi.lt"
+          name="Viktorija Ramonaitė"
+          title="Dizaino srities vadovas"
+          imgsrc={Mantas}
+          phone="+370 62 081 727"
+          email="dizainas@midi.lt"
+          placeholder={"blur"}
+        />
+        <Avatar
+          className="basis-full md:basis-1/3 lg:basis-1/4"
+          name="Viktorija Žemaitytė"
+          title="Press"
+          imgsrc={Agota}
+          phone="+370 68 307 008"
+          email="press@midi.lt"
+          placeholder={"blur"}
+        />
+        <Avatar
+          className="basis-full md:basis-1/3 lg:basis-1/4"
+          name="Martynas Mažuolis"
+          title="Video vadovas"
+          imgsrc={Rimantas}
+          phone="+370 64 479 996"
+          email="video@midi.lt"
+          placeholder={"blur"}
+        />
+        <Avatar
+          className="basis-full md:basis-1/3 lg:basis-1/4"
+          name="Laura Ukrinaitė"
+          title="Sportinio pokerio vadovė"
+          imgsrc={Mantas}
+          phone="+370 64 501 020"
+          email="pokeris@midi.lt"
+          placeholder={"blur"}
+        />
+        <Avatar
+          className="basis-full md:basis-1/3 lg:basis-1/4"
+          name="Simonas Sušickis"
+          title="Stream vadovas"
+          imgsrc={Agota}
+          phone="+370 61 679 110"
+          email="stream@midi.lt"
+          placeholder={"blur"}
+        />
+        <Avatar
+          className="basis-full md:basis-1/3 lg:basis-1/4"
+          name="Mindaugas Tolušis"
+          title="Discord vadovas"
+          imgsrc={Rimantas}
+          phone="+370 62 500 503"
+          email="discord@midi.lt"
+          placeholder={"blur"}
+        />
+        <Avatar
+          className="basis-full md:basis-1/3 lg:basis-1/4"
+          name="Mantas Kandrotas"
+          title="Offline vadovas"
+          imgsrc={Rimantas}
+          phone="+370 61 206 610"
+          email="offline@midi.lt"
+          placeholder={"blur"}
+        />
+        <Avatar
+          className="basis-full md:basis-1/3 lg:basis-1/4"
+          name="Greta Korsakaitė"
+          title="Kūrybiniai reikalai"
+          imgsrc={Rimantas}
+          phone="+370 63 096 076"
+          email="NaN"
+          placeholder={"blur"}
+        />
+        <Avatar
+          className="basis-full md:basis-1/3 lg:basis-1/4"
+          name="Ugnė Vaičiūnaitė"
+          title="Uždarymo vakaro vadovė"
+          imgsrc={Rimantas}
+          phone="+370 68 917 616"
+          email="uzdarymas@midi.lt"
+          placeholder={"blur"}
+        />
+        <Avatar
+          className="basis-full md:basis-1/3 lg:basis-1/4"
+          name="Kasparas Šumskis"
+          title="Krepšinio ir futbolo vadovas"
+          imgsrc={Rimantas}
+          phone="+370 68 923 669"
+          email="sportas@midi.lt"
+          placeholder={"blur"}
+        />
+        <Avatar
+          className="basis-full md:basis-1/3 lg:basis-1/4"
+          name="Gintarė Antulytė"
+          title="Tinklinio ir netradicinio sporto vadovė"
+          imgsrc={Rimantas}
+          phone="+370 66 223 500"
+          email="sportodiena@midi.lt"
+          placeholder={"blur"}
+        />
+        <Avatar
+          className="basis-full md:basis-1/3 lg:basis-1/4"
+          name="Aistė Jurjonaitė"
+          title="Protmūšio vadovė"
+          imgsrc={Rimantas}
+          phone="+370 64 072 522"
+          email="protmusis@midi.lt"
+          placeholder={"blur"}
+        />
+        <Avatar
+          className="basis-full md:basis-1/3 lg:basis-1/4"
+          name="Ainius Gecas"
+          title="Šachmatų turnyro vadovas"
+          imgsrc={Rimantas}
+          phone="+370 61 838 654"
+          email="sachmatai@midi.lt"
+          placeholder={"blur"}
+        />
+        <Avatar
+          className="basis-full md:basis-1/3 lg:basis-1/4"
+          name="Edvinas Burba"
+          title="Orientacinių varžybų vadovas"
+          imgsrc={Rimantas}
+          phone="+370 68 696 553"
+          email="orientacines@midi.lt"
           placeholder={"blur"}
         />
       </div>
       <p className="my-8 text-center text-6xl">MIDI Renginių vadovai</p>
       <div className="flex flex-wrap justify-center gap-8">
-        <Avatar
-          className="basis-full md:basis-1/3 lg:basis-1/4"
-          name="Mantas Mažvila"
-          title="Sportinio pokerio vadovas"
-          imgsrc={Mantas}
-          phone="+370 68 584 390"
-          email="pokeris@midi.lt"
-          placeholder={"blur"}
-        />
         <Avatar
           className="basis-full md:basis-1/3 lg:basis-1/4"
           name="Kamilė Simokaitytė"
@@ -187,33 +340,6 @@ export default function OrganizersSection() {
       </div>
       <p className="my-8 text-center text-6xl">MIDI Komunikacija</p>
       <div className="flex flex-wrap justify-center gap-8">
-        <Avatar
-          className="basis-full md:basis-1/3 lg:basis-1/4"
-          name="Martynas Mažuolis"
-          title="Video vadovas"
-          imgsrc={Martynas}
-          phone="+370 64 479 996"
-          email="video@midi.lt"
-          placeholder={"blur"}
-        />
-        <Avatar
-          className="basis-full md:basis-1/3 lg:basis-1/4"
-          name="Agota Lūžytė"
-          title="Dizaino srities vadovai"
-          imgsrc={Agota}
-          phone="+370 60 630 071"
-          email="dizainas@midi.lt"
-          placeholder={"blur"}
-        />
-        <Avatar
-          className="basis-full md:basis-1/3 lg:basis-1/4"
-          name="Rimantas Liulys"
-          title="Dizaino srities vadovas"
-          imgsrc={Rimantas}
-          phone="+370 67 888 302"
-          email="dizainas@midi.lt"
-          placeholder={"blur"}
-        />
         <Avatar
           className="basis-full md:basis-1/3 lg:basis-1/4"
           name="Redas Serafimavičius"
