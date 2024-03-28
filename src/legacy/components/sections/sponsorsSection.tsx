9340e7f6074923ee24898c5ef3220753f9128d64
Olen <olen2003@gmail.com>
2024-03-28 23:51:57 +0200

    Up to date sponsors added. Shift4 missing

--- src/legacy/components/sections/sponsorsSection.tsx
+++ src/legacy/components/sections/sponsorsSection.tsx
@@ -1,18 +1,14 @@
 import Sponsor from "../Sponsor";
 import Asseco from "../../public/sponsors/asseco.png";
-import Bankera from "../../public/sponsors/bankera.svg";
-import Wargaming from "../../public/sponsors/wargaming.png";
-import Telia from "../../public/sponsors/telia.svg";
-import WU from "../../public/sponsors/westernunion.png";
-import DreamCubator from "../../public/sponsors/dreamcubator.png";
+import Accenture from "../../public/sponsors/accenture.png";
+import TwoDay from "../../public/sponsors/twoday.svg";
+import Coherent from "../../public/sponsors/coherent.png";
+import Kpmg from "../../public/sponsors/kpmg.png";
+import WU from "../../public/sponsors/westernunionWhite.png";
 import Teltonika from "../../public/sponsors/teltonika.svg";
-import godDev from "../../public/sponsors/goddev.svg";
 import Bentley from "../../public/sponsors/bentley.png";
-import DevBridge from "../../public/sponsors/devbridge.png";
 import NordCurrent from "../../public/sponsors/nordcurrent.png";
-import MobilePay from "../../public/sponsors/mobilepay.png";
 import Luminor from "../../public/sponsors/luminor.svg";
-import TestDevLab from "../../public/sponsors/testdevlab.png";
 import MadeInVilnius from "../../public/sponsors/miv.png";
 
 var goldenSponsors = [
@@ -20,58 +16,28 @@ var goldenSponsors = [
     href: "https://lt.asseco.com/",
     src: Asseco,
     alt: "asseco logotipas",
-    className: "basis-full md:basis-1/3 scale-125",
+    className: "scale-150 sm:scale-100 basis-1/2 md:basis-1/3 px-4",
   },
   {
-    href: "https://bankerait.lt/",
-    src: Bankera,
-    alt: "Bankera logotipas",
-    className: "basis-full md:basis-1/3 scale-125",
+    href: "https://bootcamp.lv",
+    src: Accenture,
+    alt: "Accenture logotipas",
+    className: "scale-150 sm:scale-100 basis-1/2 md:basis-1/3 px-4",
   },
 ];
 
 var silverSponsors = [
   {
-    href: "https://eu.wargaming.net/",
-    src: Wargaming,
-    alt: "Wargaming logotipas",
-    className: "invert basis-1/4 scale-75 hidden",
-  },
-  {
-    href: "https://www.telia.lt/",
-    src: Telia,
-    alt: "Telia logotipas",
-    className: "basis-1/4 scale-75",
-  },
-  {
-    href: "https://www.westernunion.com/lt",
-    src: WU,
-    alt: "Western Union logotipas",
-    className: "basis-1/4 scale-75",
-  },
-  {
-    href: "https://dreamcubator.club/lt/",
-    src: DreamCubator,
-    alt: "DreamCubator logotipas",
-    className: "basis-1/4 scale-75",
+    href: "https://www.twoday.lt/",
+    src: TwoDay,
+    alt: "TwoDay logotipas",
+    className: "scale-125 basis-1/2 md:basis-1/4",
   },
   {
     href: "https://teltonika-iot-group.com/",
     src: Teltonika,
     alt: "Teltonika logotipas",
-    className: "basis-1/4 scale-75",
-  },
-  {
-    href: "https://www.god.de/en/about-us/god-lithuania/",
-    src: godDev,
-    alt: "GOD dev Lietuva logotipas",
-    className: "basis-1/4 scale-75",
-  },
-  {
-    href: "https://www.mobilepaygroup.com/about-us",
-    src: MobilePay,
-    alt: "MobilePay logotipas",
-    className: "basis-1/4",
+    className: "scale-125 basis-1/2 md:basis-1/4",
   },
 ];
 
@@ -80,12 +46,6 @@ var bronzeSponsors = [
     href: "https://www.bentley.com/",
     src: Bentley,
     alt: "Bentley logo",
-    className: "basis-1/6 scale-75",
-  },
-  {
-    href: "https://www.devbridge.com/",
-    src: DevBridge,
-    alt: "Devbridge logotipas",
     className: "basis-1/6 scale-75",
   },
   {
@@ -101,21 +61,33 @@ var bronzeSponsors = [
     className: "basis-1/6 scale-75",
   },
   {
-    href: "https://www.testdevlab.com/",
-    src: TestDevLab,
-    alt: "TestDevLab logotipas",
+    href: "https://www.coherentsolutions.com/",
+    src: Coherent,
+    alt: "Coherent logotipas",
     className: "basis-1/6 scale-75",
   },
+  {
+    href: "https://kpmg.com/lt/lt/home.html",
+    src: Kpmg,
+    alt: "Kpmg logotipas",
+    className: "basis-1/6 scale-75",
+  },
+  {
+    href: "https://careers.westernunion.com/wulithuania/",
+    src: WU,
+    alt: "Western Union logotipas",
+    className: "basis-1/6 scale-75",
+  }
 ];
 
-var partners = [
-  {
-    href: "https://madeinvilnius.lt/",
-    src: MadeInVilnius,
-    alt: "MadeInVilnius logotipas",
-    className: "scale-75",
-  },
-];
+// var partners = [
+//   {
+//     href: "https://madeinvilnius.lt/",
+//     src: MadeInVilnius,
+//     alt: "MadeInVilnius logotipas",
+//     className: "scale-75",
+//   },
+// ];
 
 export default function SponsorsSection() {
   return (
@@ -159,7 +131,7 @@ export default function SponsorsSection() {
         ))}
       </div>
       <div className="my-4 h-2 bg-slate-600/30"></div>
-      <p className="text-center text-5xl md:text-7xl">Partneriai</p>
+      {/* <p className="text-center text-5xl md:text-7xl">Partneriai</p>
       <div className="m-8 flex flex-wrap place-items-center justify-around justify-items-center gap-8 align-middle">
         {partners.map((sponsor, idx) => (
           <Sponsor
@@ -170,7 +142,7 @@ export default function SponsorsSection() {
             className={sponsor.className}
           ></Sponsor>
         ))}
-      </div>
+      </div> */}
     </div>
   );
 }
