cac46d692fba6d894d6814058948b47cb89351dd
Domas Klimavicius <dev@dklimavicius.com>
2024-03-30 14:21:00 +0200

    Add standard sponsor tier, replace placeholder sponsors with real ones

--- src/components/sponsors/Sponsors.tsx
+++ src/components/sponsors/Sponsors.tsx
@@ -3,9 +3,9 @@ import { SponsorType } from "./Types.ts"
 import './styles.css'
 
 export default function Sponsors() {
-    // TODO: replace with real sponsonrs
-    // placeholder data
-    const sponsors: { gold: SponsorType[], silver: SponsorType[], bronze: SponsorType[] } = {
+    const sponsors: { gold: SponsorType[], silver: SponsorType[], bronze: SponsorType[], standard: SponsorType[] } = {
+
+        // TODO: change logos
         gold: [
             {
                 title: 'asseco',
@@ -13,55 +13,20 @@ export default function Sponsors() {
                 logo: 'https://midi.lt/2023/_next/static/media/asseco.bd0fe713.png?w=3840&q=75'
             },
             {
-                title: 'bankera',
-                url: 'https://bankerait.lt/',
-                logo: 'https://midi.lt/2023/_next/static/media/bankera.5825ccf0.svg?w=1200&q=75'
-            },
-            {
-                title: 'asseco1',
-                url: 'https://lt.asseco.com/',
+                title: 'accenture',
+                url: 'https://www.accenture.com/',
                 logo: 'https://midi.lt/2023/_next/static/media/asseco.bd0fe713.png?w=3840&q=75'
-            },
-            {
-                title: 'bankera1',
-                url: 'https://bankerait.lt/',
-                logo: 'https://midi.lt/2023/_next/static/media/bankera.5825ccf0.svg?w=1200&q=75'
-            },
-            {
-                title: 'asseco2',
-                url: 'https://lt.asseco.com/',
-                logo: 'https://midi.lt/2023/_next/static/media/asseco.bd0fe713.png?w=3840&q=75'
-            },
-            {
-                title: 'bankera2',
-                url: 'https://bankerait.lt/',
-                logo: 'https://midi.lt/2023/_next/static/media/bankera.5825ccf0.svg?w=1200&q=75'
-            },
-            {
-                title: 'asseco3',
-                url: 'https://lt.asseco.com/',
-                logo: 'https://midi.lt/2023/_next/static/media/asseco.bd0fe713.png?w=3840&q=75'
-            },
-            {
-                title: 'bankera3',
-                url: 'https://bankerait.lt/',
-                logo: 'https://midi.lt/2023/_next/static/media/bankera.5825ccf0.svg?w=1200&q=75'
-            },
-            {
-                title: 'bankera4',
-                url: 'https://bankerait.lt/',
-                logo: 'https://midi.lt/2023/_next/static/media/bankera.5825ccf0.svg?w=1200&q=75'
             },
         ],
         silver: [
             {
-                title: 'telia',
-                url: 'https://www.telia.lt/',
+                title: 'twoday',
+                url: 'https://www.twoday.lt/',
                 logo: 'https://midi.lt/2023/_next/static/media/telia.84485178.svg?w=828&q=75'
             },
             {
-                title: 'wargaming',
-                url: 'https://wgforge.wargaming.com/',
+                title: 'teltonika',
+                url: 'https://teltonika-iot-group.com/lt',
                 logo: 'https://midi.lt/2023/_next/static/media/wargaming.b4a13829.svg?w=640&q=75'
             },
         ],
@@ -72,9 +37,56 @@ export default function Sponsors() {
                 logo: 'https://midi.lt/2023/_next/static/media/bentley.8296207b.png?w=640&q=75'
             },
             {
-                title: 'cognizant',
-                url: 'https://www.devbridge.com/',
-                logo: 'https://midi.lt/2023/_next/static/media/devbridge.9aae1ed8.png?w=1920&q=75'
+                title: 'nordcurrent',
+                url: 'https://nordcurrent.com/',
+                logo: 'https://midi.lt/2023/_next/static/media/bentley.8296207b.png?w=640&q=75'
+            },
+            {
+                title: 'luminor',
+                url: 'https://www.luminor.lt/',
+                logo: 'https://midi.lt/2023/_next/static/media/bentley.8296207b.png?w=640&q=75'
+            },
+            {
+                title: 'coherent_solutions',
+                url: 'https://www.coherentsolutions.com/',
+                logo: 'https://midi.lt/2023/_next/static/media/bentley.8296207b.png?w=640&q=75'
+            },
+            {
+                title: 'kpmg_baltics',
+                url: 'https://kpmg.com/lt/en/home.html',
+                logo: 'https://midi.lt/2023/_next/static/media/bentley.8296207b.png?w=640&q=75'
+            },
+            {
+                title: 'western_union',
+                url: 'https://www.westernunion.com/lt/en/home.html',
+                logo: 'https://midi.lt/2023/_next/static/media/bentley.8296207b.png?w=640&q=75'
+            },
+            {
+                title: 'shift4',
+                url: 'https://www.shift4.com/',
+                logo: 'https://midi.lt/2023/_next/static/media/bentley.8296207b.png?w=640&q=75'
+            },
+        ],
+        standard: [
+            {
+                title: 'pwc',
+                url: 'https://www.pwc.com/lt/lt/',
+                logo: 'https://midi.lt/2023/_next/static/media/bentley.8296207b.png?w=640&q=75'
+            },
+            {
+                title: 'wix',
+                url: 'https://www.wix.com/',
+                logo: 'https://midi.lt/2023/_next/static/media/bentley.8296207b.png?w=640&q=75'
+            },
+            {
+                title: 'vtex',
+                url: 'https://vtex.lt/',
+                logo: 'https://midi.lt/2023/_next/static/media/bentley.8296207b.png?w=640&q=75'
+            },
+            {
+                title: 'neara',
+                url: 'https://neara.com/',
+                logo: 'https://midi.lt/2023/_next/static/media/bentley.8296207b.png?w=640&q=75'
             },
         ]
     }
@@ -99,6 +111,13 @@ export default function Sponsors() {
                 <p className="mb-8 text-center text-5xl md:text-7xl text-white sponsor--font">Bronziniai rėmėjai</p>
                 <div className="flex flex-wrap justify-center">
                     {sponsors.bronze.map(sponsor =>
+                        (<Sponsor key={sponsor.title} sponsor={sponsor}/>)
+                    )}
+                </div>
+                <div className="my-4 h-2 bg-slate-600/30" style={{width: "98%", margin: "auto"}}></div>
+                <p className="mb-8 text-center text-5xl md:text-7xl text-white sponsor--font">Standartiniai rėmėjai</p>
+                <div className="flex flex-wrap justify-center">
+                    {sponsors.standard.map(sponsor =>
                         (<Sponsor key={sponsor.title} sponsor={sponsor}/>)
                     )}
                 </div>
