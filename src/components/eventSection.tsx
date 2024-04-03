a42e24fae84025e02bfbb9d849465d80591c3212
Olen <olen2003@gmail.com>
2024-04-03 18:40:14 +0300

    Finalized text in events
    Co-authored-by: Dax0s <Dax0s@users.noreply.github.com>

--- src/components/eventSection.tsx
+++ src/components/eventSection.tsx
@@ -1,24 +1,16 @@
-import React, { useState, useEffect, useRef } from "react";
-import placeholderImg from "../images/placeholderEvent.webp";
-import placeholderZoom from "../images/placeholderZoom.png";
+import { useState, useEffect, useRef, SetStateAction } from "react";
 import placeholderBanner from "../images/team_clipboard.png";
-import RedNote from "../images/redNote.png";
-import GreenNote from "../images/greenNote.png";
-import OrangeNote from "../images/orangeNote.png";
-import PurpleNote from "../images/purpleNote.png";
-import CatNote from "../images/catTest.webp";
-import BlankNote from "../images/noteBlank.svg";
+import BlankNote from "../images/noteBlankTest.png";
 import LANNote from "../images/noteLan.svg";
 import DecathlonNote from "../images/noteDecathlon.svg";
 import ImoniuMugeNote from "../images/noteImoniuMuge.svg";
 import OrientacinesNote from "../images/noteOrientacines.svg";
 import ProtmusisNote from "../images/noteProtmusis.svg";
-import RONote from "../images/noteRO.svg";
 import SachmataiNote from "../images/noteSachmatai.svg";
 import SportodienosNote from "../images/noteSportodienos.svg";
 import UzdarymasNote from "../images/noteUzdarymas.svg";
 import LANBanner from "../images/lanparty.webp";
-import TeamClipboard from "../images/clipboard-01.png";
+import TeamClipboard from "../images/clipboard-02.png";
 
 export default function EventSection() {
   const [activeNote, setActiveNote] = useState(null);
@@ -51,7 +43,43 @@ export default function EventSection() {
       eventName: "MIDI Lan Party",
       image: LANNote,
       banner: LANBanner,
-      text: "Testuojamas tekstas note'e nomer viens",
+      text: `👾 Margi pikseliai, klavišų garsas, pelės paspaudimai, žaidimo kauliukai, žetonų barškėjimas - ar esi pasiruošęs patirti visa tai ir daugiau vienoje vietoje?<br>
+      <br>
+      👉 MIDI kviečia visus video žaidimų, stalo žaidimų, sportinio pokerio entuziastus ir mėgėjus balandžio 20-21 dienomis dalyvauti kasmetiniame MIDI LAN Party ir laimėti įvairių prizų!<br>
+      <br>
+      🎮 Visus komandinę dvasią jaučiančius žaidėjus kviečiame burti komandas ir išbandyti savo jėgas pagrindiniuose LAN turnyruose - League Of Legends, Valorant ir Counter-Strike 2. Nepamiršk pakviesti ir palaikymo komandos, nes renginio metu bus užtikrintos tribūnos palaikymui!<br>
+      <br>
+      🎯 Jeigu pagrindiniai LAN turnyrai tavęs nesužavėjo ar pilnai nepasitiki savo įgūdžiais – nenusimink! Tavęs lauks įvairūs kompiuteriniai žaidimai - Sims 4 build-offs, Tetris, Tekken 7, Teamfight Tactics ir kiti. Kaip ir pagrindiniuose turnyruose, čia tavęs taip pat laukia prizai.<br>
+      <br>
+      🎲 Galbūt nesi virtualaus pasaulio pilietis, bet mėgsti žaisti stalo žaidimus? LAN Party metu galėsi išbandyti savo jėgas žaisdamas tokius žaidimus kaip Wingspan ar Powergrid ir, žinoma, laimėti prizų. Taip pat tavęs lauks ir didelė įvairovė kitų stalo žaidimų.<br>
+      <br>
+      🃏 Mėgsti kortų žaidimus ir nori išbandyti savo sėkmę? LAN Party metu turėsi galimybę ištraukti savo laimingą kortą ir sudalyvauti sportinio pokerio turnyre!<br>
+      <br>
+      🧩 O kad nepabostų tik konkuruoti dėl prizų, viso renginio metu tavęs laukia ir laisvoji zona, kur su bendraminčiais galėsi kartu žaisti, ką tik panorėjęs. Taip pat galėsi pajudėti virtualiame pasaulyje - esančiame VR zonoje - ar išbandyti savo šokių sugebėjimus visų pamiltame Just Dance!<br>
+      <br>
+      ‼️ SVARBU ‼️<br>
+      LAN party kompiuterinių žaidimų turnyrams reikalinga sava įranga - kompiuteris ir visa reikalinga periferinė įranga - monitoriai, pelės, klaviatūros ir panašiai, tad planuojant dalyvauti bet kuriame kompiuterinių žaidimų turnyre, nepamiršk pasiimti reikalingos įrangos!<br>
+      <br>
+      Pačiame renginyje gyvai privalo dalyvauti bent 60% turnyruose dalyvaujančių komandos žaidėjų.<br>
+      <br>
+      🇬🇧<br>
+      <br>
+      👾 Colorful pixels, sound of keyboards, mouse clicks, game dice, rattling of poker chips – are you ready to experience all of it and more in one place?<br>
+      <br>
+      👉 MIDI is inviting all video game, board game, sports poker enthusiasts and enjoyers to participate in the annual MIDI LAN Party on April 20-21 and win various prizes!<br>
+      <br>
+      🎮 All players with a sense of team spirit are invited to form teams and test their strength in the main LAN tournaments – League Of Legends, Valorant and Counter-Strike 2. Don’t forget to invite the support team as well, as there will be stands provided for the supporters during the event!<br>
+      <br>
+      🎯 But if the main LAN tournaments didn’t catch your attention or you simply are not confident enough in your skills – don’t be upset! Various other computer games will be waiting for you – Sims 4 build-offs, Tetris, Tekken 7, Teamfight Tactics and more. Just like the main tournaments, there will be prizes waiting for you.<br>
+      <br>
+      🎲 Perhaps you’re not an often visitor to the virtual world, but like to play board games? During LAN party, you can try your hand at games like Wingspan or Powergrid and, of course, win prizes. A wide variety of other boards games will also await you.<br>
+      <br>
+      🃏 Love card games and want to test your luck? During LAN party, you will have an opportunity to draw your lucky card and participate in Sports Poker Tournament!<br>
+      <br>
+      🧩 There will also be a free zone available for the entire event, so that you don’t get bored of just competing for the prizes, where you can play whatever, you wish together with other like-minded people. You will also have a chance to explore the virtual world – VR zone – or test your dancing skills in everyone’s beloved Just Dance!<br>
+      <br>
+      ‼️ IMPORTANT ‼️<br>
+      LAN party computer game tournaments require your own equipment – a computer and all necessary peripheral equipment – monitors, computer mice, keyboards, and such, so when planning to participate in any of the tournaments, do not forget to take the necessary equipment! At least 60% of the team players participating in the tournaments must participate live in the event itself.<br>`,
       rotate: "rotate(-10deg)",
       clickable: true,
     },
@@ -292,8 +320,7 @@ export default function EventSection() {
           >
             {/* Note container with adjusted smaller dimensions */}
             <div
-              className="relative transform -rotate-2"
-              style={{ width: "35vw", height: "55vh" }} // Slightly reduced dimensions
+              className="relative transform -rotate-2 w-[25vw] h-full" // Slightly reduced dimensions
             >
               <img
                 src={BlankNote}
@@ -301,11 +328,11 @@ export default function EventSection() {
                 className="w-full h-full object-cover" // Ensures the note covers the specified dimensions
               />
               {/* Overlay content for the note */}
-              <div className="absolute inset-0 flex flex-col justify-center items-center space-y-3 p-4">
+              <div className="absolute inset-0 flex flex-col justify-center items-center space-y-3 p-4 w-[100%]">
                 <img
                   src={notes.find((note) => note.id === activeNote)?.banner}
                   alt="Event Banner"
-                  className="object-cover h-[15vh]" // Adjust the size to maintain proportionality
+                  className="object-cover h-[45%]" // Adjust the size to maintain proportionality
                 />
                 <button className="py-1 px-3 text-xs bg-blue-500 text-white rounded-full">
                   Daugiau / Read More
@@ -315,24 +342,26 @@ export default function EventSection() {
                 </button>
               </div>
             </div>
-
             <div
-              className="relative transform rotate-2"
-              style={{ width: "35vw", height: "60vh" }} // Keeping the clipboard slightly smaller
+              className="relative transform rotate-2 w-[35vw] h-full" // Keeping the clipboard slightly smaller
             >
               <img
                 src={TeamClipboard}
                 alt="Clipboard"
-                className="w-full h-full"
+                className="object-cover h-full"
               />
               {/* Text overlay for the clipboard */}
-              <div
-                className="absolute inset-0 p-4 flex justify-center items-start"
-                style={{ paddingTop: "10%" }}
-              >
-                <p className="text-white text-xs md:text-sm">
-                  {notes.find((note) => note.id === activeNote)?.text}
-                </p>
+              <div className="absolute inset-0 p-0 flex justify-center items-start">
+                <div className="absolute top-[10%] px-12 pb-10 overflow-auto h-[85%] no-scrollbar">
+                  <p
+                    className="sacramento-regular text-black text-lg h-[100%]"
+                    dangerouslySetInnerHTML={{
+                      __html:
+                        notes.find((note) => note.id === activeNote)?.text ??
+                        "",
+                    }}
+                  />
+                </div>
               </div>
             </div>
           </div>
