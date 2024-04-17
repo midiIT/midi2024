1cdcb83f8a218ab79c9715fcf3ed7ef07dd392f5
tikis23 <edvis01x@gmail.com>
2024-04-17 11:30:49 +0300

    Spotlight effect

--- src/pages/interactive.tsx
+++ src/pages/interactive.tsx
@@ -1,15 +1,20 @@
+import { useState, MouseEvent } from "react";
 import TeamSection from "../components/interactive/teamSection.tsx";
 import EventSection from "../components/interactive/eventSection.tsx";
 import Sponsors from "../components/interactive/sponsors/Sponsors.tsx";
 import MIDILogo from "../images/interactive/MIDI-Logotipas.png";
 
-
 function Interactive() {
+  const [cursorPosition, setCursorPosition] = useState({ top: 0, left: 0 })
+
+  const onMouseMove = (e: MouseEvent<HTMLDivElement>) => {
+      setCursorPosition({ top: e.clientY - e.currentTarget.offsetTop, left: e.clientX - e.currentTarget.offsetLeft});
+  };
 
   return (
     <>
       {/* Corkboard Section */}
-      <div className="relative h-screen bg-cover bg-center bg-[url(../images/interactive/corkboard.jpg)]">
+      <div onMouseMove={onMouseMove} className="relative h-screen bg-cover bg-center bg-[url(../images/interactive/corkboard.jpg)]">
         {/* Logo Container */}
         <div className="flex justify-center pt-4">
           <img className="h-32 w-32" src={MIDILogo} alt="MIDI Logo"></img>
@@ -20,6 +25,10 @@ function Interactive() {
           <div className="flex flex-col absolute bottom-4 right-4 items-center">
             <TeamSection />
           </div>
+
+        {/* Spotlight Effect */}
+        <div style={{background: `radial-gradient(circle at ${cursorPosition.left}px ${cursorPosition.top}px, #00000000 10px, #000000ee 350px)`,
+                    pointerEvents: `none`}} className="z-10 absolute inset-0 top-0 left-0"/>
       </div>
 
       {/* Sponsors Section */}
